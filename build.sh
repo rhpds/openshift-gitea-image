95b7070711d7d12f673eab79bab86928f4fb5238
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-04-17 09:31:02 -0400

    Added git tag/push

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-docker build . -t docker.io/wkulhanek/gitea:latest
-docker tag docker.io/wkulhanek/gitea:latest docker.io/wkulhanek/gitea:1.4
+VERSION=1.4
+docker build . -t docker.io/wkulhanek/gitea:${VERSION}
+docker tag docker.io/wkulhanek/gitea:${VERSION} docker.io/wkulhanek/gitea:latest
+docker push docker.io/wkulhanek/gitea:${VERSION}
 docker push docker.io/wkulhanek/gitea:latest
-docker push docker.io/wkulhanek/gitea:1.4
+git tag ${VERSION}
+git push origin ${VERSION}
