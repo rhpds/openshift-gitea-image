d21dbb9996f02277a2ad8717671ef0e5bbec33b7
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-01-24 10:57:57 +0000

    Updated for Gitea 1.7.0

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.6.3
+VERSION=1.7.0
 docker build . -t docker.io/wkulhanek/gitea:${VERSION}
 docker tag docker.io/wkulhanek/gitea:${VERSION} docker.io/wkulhanek/gitea:latest
 docker push docker.io/wkulhanek/gitea:${VERSION}
