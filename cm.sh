65ca5a40b80963f4ca4a5fe81ee76d454cdee360
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-01-24 15:29:41 +0000

    Removed extra files

--- cm.sh
+++
@@ -1,3 +0,0 @@
-oc exec $(oc get pod | grep "^gitea" | awk '{print $1}') -- cat /home/gitea/conf/app.ini >app.ini
-oc create configmap gitea --from-file=app.ini
-oc set volume dc/gitea --add --overwrite --name=config-volume -m /home/gitea/conf/ -t configmap --configmap-name=gitea
