2f5e681edc743528f3a127b4348c45f0191b0b08
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-04-23 19:52:40 -0400

    Updated to Gitea 1.8.0

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.7.5
+VERSION=1.8.0
 docker build . -t docker.io/wkulhanek/gitea:${VERSION}
 docker tag docker.io/wkulhanek/gitea:${VERSION} docker.io/wkulhanek/gitea:latest
 docker push docker.io/wkulhanek/gitea:${VERSION}
