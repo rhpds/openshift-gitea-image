c40c9f1005357a50ef90844e8ef0cbfadd0b5263
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-07-31 17:53:48 -0400

    Updated to push to gpte-devops-automation rather than personal repo

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 VERSION=1.9.0
-docker build . -t docker.io/wkulhanek/gitea:${VERSION}
-docker tag docker.io/wkulhanek/gitea:${VERSION} docker.io/wkulhanek/gitea:latest
-docker push docker.io/wkulhanek/gitea:${VERSION}
-docker push docker.io/wkulhanek/gitea:latest
+docker build . -t quay.io/gpte-devops-automation/gitea:${VERSION}
+docker tag quay.io/gpte-devops-automation/gitea:${VERSION} quay.io/gpte-devops-automation/gitea:latest
+docker push quay.io/gpte-devops-automation/gitea:${VERSION}
+docker push quay.io/gpte-devops-automation/gitea:latest
 git tag ${VERSION}
 git push origin ${VERSION}
