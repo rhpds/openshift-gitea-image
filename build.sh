14aacde00dad10271792c1d05fbb23c71c257f5c
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-08-26 16:32:54 -0400

    Updated for 1.9.2

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.9.1
+VERSION=1.9.2
 docker build . -t quay.io/gpte-devops-automation/gitea:${VERSION}
 docker tag quay.io/gpte-devops-automation/gitea:${VERSION} quay.io/gpte-devops-automation/gitea:latest
 docker push quay.io/gpte-devops-automation/gitea:${VERSION}
