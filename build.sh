aa9ab9924acf492675c4591e920f012d5d8433a4
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2021-09-03 09:04:46 -0400

    Changed to podman, updated for 1.15.1

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-VERSION=1.15.0
-docker build . -t quay.io/gpte-devops-automation/gitea:${VERSION}
-docker tag quay.io/gpte-devops-automation/gitea:${VERSION} quay.io/gpte-devops-automation/gitea:latest
-docker push quay.io/gpte-devops-automation/gitea:${VERSION}
-docker push quay.io/gpte-devops-automation/gitea:latest
+VERSION=1.15.1
+podman build . -t quay.io/gpte-devops-automation/gitea:${VERSION}
+podman tag quay.io/gpte-devops-automation/gitea:${VERSION} quay.io/gpte-devops-automation/gitea:latest
+podman push quay.io/gpte-devops-automation/gitea:${VERSION}
+podman push quay.io/gpte-devops-automation/gitea:latest
 git tag ${VERSION}
 git push origin ${VERSION}
