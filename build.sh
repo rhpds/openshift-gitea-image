e62c16a6574c1e1421e39407b669a902207e33e2
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2022-03-17 09:39:29 -0400

    Updated for Gitea 1.16.4

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.16.3
+VERSION=1.16.4
 podman build . -t quay.io/gpte-devops-automation/gitea:${VERSION}
 podman tag quay.io/gpte-devops-automation/gitea:${VERSION} quay.io/gpte-devops-automation/gitea:latest
 podman push quay.io/gpte-devops-automation/gitea:${VERSION}
